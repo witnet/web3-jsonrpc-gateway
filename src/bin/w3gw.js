cbbf999388c15d3a43f85262c2ffded1ba717b7f
Guillermo Díaz <guillermo@otherplane.com>
2023-06-14 14:47:51 +0200

    chore: attend PR review comments

--- src/bin/w3gw.js
+++ src/bin/w3gw.js
@@ -3,61 +3,86 @@ require('dotenv').config()
 const execSync = require('child_process').execSync
 const scripts = require('../../package.json').scripts
 
-if (process.argv.length >= 3 && (process.env.W3GW_SEED_PHRASE || process.env.W3GW_PRIVATE_KEYS)) {
+if (process.argv.length >= 3) {
+  // search for network and launch gateway, if found
+  let ecosystem 
   for (var key in scripts) {
-    if (key === process.argv[2]) {
-      if (process.argv.length >= 4) {
-        // a specific JSONRPC provider has been specified in the command line:
-        var cmdline = scripts[key].split(' ')
-        cmdline[cmdline.length - 2] = process.argv[3]
-        if (process.env.W3GW_PORT) {
+    if (key.indexOf(":") > -1 && key === process.argv[2]) {
+      if (process.env.W3GW_SEED_PHRASE || process.env.W3GW_PRIVATE_KEYS) {
+        if (process.argv.length >= 4) {
+          // a specific JSONRPC provider has been specified in the command line:
+          var cmdline = scripts[key].split(' ')
+          cmdline[cmdline.length - 2] = process.argv[3]
+          if (process.env.W3GW_PORT) {
+            cmdline[cmdline.length - 1] = process.env.W3GW_PORT
+          }
+          execSync(
+            'yarn '
+              .concat(cmdline.join(' '), ' ')
+              .concat(process.argv.slice(4).join(' ')),
+            { stdio: 'inherit' }
+          )
+        } else if (process.env.W3GW_PROVIDER_URL) {
+          // the W3GW_PROVIDER_URL variable is set
+          var cmdline = scripts[key].split(' ')
+          cmdline[cmdline.length - 2] = process.env.W3GW_PROVIDER_URL
+          if (process.env.W3GW_PORT) {
+            cmdline[cmdline.length - 1] = process.env.W3GW_PORT
+          }
+          execSync(
+            'yarn '
+              .concat(cmdline.join(' '), ' ')
+              .concat(process.argv.slice(3).join(' ')),
+            { stdio: 'inherit' }
+          )
+        } else if (process.env.W3GW_PORT) {
+          // the W3GW_PORT variable is set while W3GW_PROVIDER_URL is not
+          var cmdline = scripts[key].split(' ')
           cmdline[cmdline.length - 1] = process.env.W3GW_PORT
+          execSync(
+            'yarn '
+              .concat(cmdline.join(' '), ' ')
+              .concat(process.argv.slice(3).join(' ')),
+            { stdio: 'inherit' }
+          )
+        } else {
+          execSync(
+            'yarn '
+              .concat(
+                scripts[key].replace(
+                  '$W3GW_PROVIDER_KEY',
+                  process.env.W3GW_PROVIDER_KEY
+                ),
+                ' '
+              )
+              .concat(process.argv.slice(3).join(' ')),
+            { stdio: 'inherit' }
+          )
         }
-        execSync(
-          'yarn '
-            .concat(cmdline.join(' '), ' ')
-            .concat(process.argv.slice(4).join(' ')),
-          { stdio: 'inherit' }
-        )
-      } else if (process.env.W3GW_PROVIDER_URL) {
-        // the W3GW_PROVIDER_URL variable is set
-        var cmdline = scripts[key].split(' ')
-        cmdline[cmdline.length - 2] = process.env.W3GW_PROVIDER_URL
-        if (process.env.W3GW_PORT) {
-          cmdline[cmdline.length - 1] = process.env.W3GW_PORT
-        }
-        execSync(
-          'yarn '
-            .concat(cmdline.join(' '), ' ')
-            .concat(process.argv.slice(3).join(' ')),
-          { stdio: 'inherit' }
-        )
-      } else if (process.env.W3GW_PORT) {
-        // the W3GW_PORT variable is set while W3GW_PROVIDER_URL is not
-        var cmdline = scripts[key].split(' ')
-        cmdline[cmdline.length - 1] = process.env.W3GW_PORT
-        execSync(
-          'yarn '
-            .concat(cmdline.join(' '), ' ')
-            .concat(process.argv.slice(3).join(' ')),
-          { stdio: 'inherit' }
-        )
+        process.exit(0)
       } else {
-        execSync(
-          'yarn '
-            .concat(
-              scripts[key].replace(
-                '$W3GW_PROVIDER_KEY',
-                process.env.W3GW_PROVIDER_KEY
-              ),
-              ' '
-            )
-            .concat(process.argv.slice(3).join(' ')),
-          { stdio: 'inherit' }
-        )
+        console.info()
+        console.info("Cannot launch", key, "gateway !!")
+        console.info("Please, setup the W3GW_SEED_PHRASE environment variable, or add it to the .env file!")
+        process.exit(1)
       }
-      process.exit(0)
+    } else if (key.indexOf(":") && key.split(":")[0].toLowerCase() === process.argv[2].toLowerCase()) {
+      ecosystem = process.argv[2].toLowerCase()
+      break
     }
+  }
+  // if parameter matched a known ecosystem, list available network within it
+  if (ecosystem) {
+    const header = `AVAILABLE NETWORKS ON ${ecosystem.toUpperCase()}`
+    console.info()
+    console.info(header)
+    console.info("=".repeat(header.length))
+    for (var key in scripts) {
+      if (key.split(":")[0].toLowerCase() === ecosystem) {
+        console.info('  ', key)
+      }
+    }
+    process.exit(0)
   }
 }
 console.info('Usage:')
@@ -66,10 +91,13 @@ console.info(
   '  ',
   '$ '
     .concat(process.argv[0], ' ')
-    .concat(process.argv[1], ' <ecosystem>:<network>')
+    .concat(process.argv[1], ' [<ecosystem>[:<network>] [custom-rpc-provider-url]]')
 )
 console.info()
-console.info('Supported values:')
+
+const header = "AVAILABLE NETWORKS"
+console.info("  ", header)
+console.info("  ", "=".repeat(header.length))
 console.info()
 for (var key in scripts) {
   if (key.indexOf(':') > -1) {
@@ -89,7 +117,7 @@ console.info(
 )
 console.info()
 console.info(
-  'Optionally, you can specify an ETH/JSONRPC endpoint different from the default one by setting:'
+  'Optionally, you can specify a custom ETH/JSONRPC endpoint by setting:'
 )
 console.info()
 console.info(
